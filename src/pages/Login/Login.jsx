bf7c74157938eda52a7bad4e6b0f114edbb9af0c
adamrafiffaqih <90492899+AdamRFaqih@users.noreply.github.com>
2023-10-20 17:56:14 +0700

    Update Login.jsx

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,14 +1,31 @@
+import { useState } from 'react';
+import { useNavigate } from "react-router-dom";
 import logo from '/src/assets/LogoASE-Text.png'
 
 export default function Login(){
+    const [formData, setFormData] = useState({
+        username: "", password: ""
+    });
+    const navigate = useNavigate()
+    function handleLogin(e){
+        e.preventDefault()
+        navigate("/")
+    }
+    const submitAvailable = formData.username&&formData.password;
+    function updateFormData(e){
+        setFormData({
+            ...formData,
+            [e.target.name]:e.target.value
+        });
+    }
     return (
         <div class="flex flex-col justify-center items-center">
             <img src={logo} alt="LogoASE" className="w-96" />
             <div class="login">
-                <form className="flex flex-col gap-5" onSubmit={(e)=>e.preventDefault()}>
-                    <input type="text" placeholder="Username"/>
-                    <input type="password" placeholder="Password"/>
-                    <input type="submit" className="ASE-button bg-aseorange"/>
+                <form className="flex flex-col gap-5 items-center" onSubmit={handleLogin}>
+                    <input type="text" onChange={updateFormData} name="username" placeholder="Username" className="p-2 border rounded-xl border-black drop-shadow-lg shadow-black"/>
+                    <input type="password" onChange={updateFormData} name="password" placeholder="Password" className="p-2 border rounded-xl border-black drop-shadow-lg shadow-black"/>
+                    <input type="submit" value="Login" disabled={!submitAvailable} className={" h-12 w-36 rounded-xl " + (submitAvailable ? "bg-aseorange  text-white": "bg-white text-black border border-asegrey")}/>
                 </form>
             </div>
         </div>
