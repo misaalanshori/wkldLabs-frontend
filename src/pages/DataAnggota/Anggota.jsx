1384d32d33befc119f4b5cab6733bb3c5e92aa16
BlyatMan221 <119160448+HelloWorldYessir420@users.noreply.github.com>
2023-10-25 18:18:42 +0700

    WEBPRO-15
    git commit -m "WEBPRO-15 <message>"

---
+++ src/pages/DataAnggota/Anggota.jsx
@@ -0,0 +1,103 @@
+import { Table, TableHeader, TableBody, TableRow, TableCol } from '../../components/Table';
+import profile from "../../assets/dummy-profile.jpg";
+import { MdOutlineDelete, MdMoreVert } from "react-icons/md";
+
+export default function Anggota() {
+    return (
+        <div className="p-4 w-full flex flex-col" >
+            <h1 className="text-3xl font-medium my-5 mx-5">Data Anggota</h1>
+            <Table>
+                <TableHeader>
+                    <TableCol></TableCol>
+                    <TableCol>Name</TableCol>
+                    <TableCol>Faculty</TableCol>
+                    <TableCol>Major</TableCol>
+                    <TableCol>Entry Year</TableCol>
+                    <TableCol>Age</TableCol>
+                    <TableCol></TableCol>
+                </TableHeader>
+                <TableBody>
+                    <TableRow>
+                        <TableCol><img class="w-10 aspect-square rounded-full place-content-center" src={profile} /></TableCol>
+                        <TableCol>Muhammad Isa Al Anshori</TableCol>
+                        <TableCol>FIF</TableCol>
+                        <TableCol>RPL</TableCol>
+                        <TableCol>2021</TableCol>
+                        <TableCol>20</TableCol>
+                        <TableCol><span className="ml-auto mr-8 w-fit flex flex-row gap-2">
+                            <MdOutlineDelete size={24} />
+                            <MdMoreVert size={24} />
+                        </span>
+                        </TableCol>
+                    </TableRow>
+                    <TableRow>
+                        <TableCol><img class="w-10 aspect-square rounded-full place-content-center" src={profile} /></TableCol>
+                        <TableCol>Adam Rafif Faqih</TableCol>
+                        <TableCol>FIF</TableCol>
+                        <TableCol>RPL</TableCol>
+                        <TableCol>2021</TableCol>
+                        <TableCol>20</TableCol>
+                        <TableCol><span className="ml-auto mr-8 w-fit flex flex-row gap-2">
+                            <MdOutlineDelete size={24} />
+                            <MdMoreVert size={24} />
+                        </span>
+                        </TableCol>
+                    </TableRow>
+                    <TableRow>
+                        <TableCol><img class="w-10 aspect-square rounded-full place-content-center" src={profile} /></TableCol>
+                        <TableCol>Maximus Bayu</TableCol>
+                        <TableCol>FIF</TableCol>
+                        <TableCol>RPL</TableCol>
+                        <TableCol>2021</TableCol>
+                        <TableCol>20</TableCol>
+                        <TableCol><span className="ml-auto mr-8 w-fit flex flex-row gap-2">
+                            <MdOutlineDelete size={24} />
+                            <MdMoreVert size={24} />
+                        </span>
+                        </TableCol>
+                    </TableRow>
+                    <TableRow>
+                        <TableCol><img class="w-10 aspect-square rounded-full place-content-center" src={profile} /></TableCol>
+                        <TableCol>Muhamad Ilham Nasrullah</TableCol>
+                        <TableCol>FIF</TableCol>
+                        <TableCol>RPL</TableCol>
+                        <TableCol>2021</TableCol>
+                        <TableCol>20</TableCol>
+                        <TableCol><span className="ml-auto mr-8 w-fit flex flex-row gap-2">
+                            <MdOutlineDelete size={24} />
+                            <MdMoreVert size={24} />
+                        </span>
+                        </TableCol>
+                    </TableRow>
+                    <TableRow>
+                        <TableCol><img class="w-10 aspect-square rounded-full place-content-center" src={profile} /></TableCol>
+                        <TableCol>Habli Zulvana Ath-Thaariq</TableCol>
+                        <TableCol>FIF</TableCol>
+                        <TableCol>RPL</TableCol>
+                        <TableCol>2021</TableCol>
+                        <TableCol>20</TableCol>
+                        <TableCol><span className="ml-auto mr-8 w-fit flex flex-row gap-2">
+                            <MdOutlineDelete size={24} />
+                            <MdMoreVert size={24} />
+                        </span>
+                        </TableCol>
+                    </TableRow>
+                    <TableRow>
+                        <TableCol><img class="w-10 aspect-square rounded-full place-content-center" src={profile} /></TableCol>
+                        <TableCol>Nabiel Prayoga</TableCol>
+                        <TableCol>FIF</TableCol>
+                        <TableCol>RPL</TableCol>
+                        <TableCol>2021</TableCol>
+                        <TableCol>20</TableCol>
+                        <TableCol><span className="ml-auto mr-8 w-fit flex flex-row gap-2">
+                            <MdOutlineDelete size={24} />
+                            <MdMoreVert size={24} />
+                        </span>
+                        </TableCol>
+                    </TableRow>
+                </TableBody>
+            </Table>
+                <button class="item-right self-end border-2 bg-white hover:bg-asegrey text-black font-normal px-9 py-1 my-10 rounded-full">New Member</button>
+        </div>
+    )
+}
