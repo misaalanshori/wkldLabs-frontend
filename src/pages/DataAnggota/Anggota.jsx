226bca9a181ba2758270d888da865887d4c7bc9b
BlyatMan221 <119160448+HelloWorldYessir420@users.noreply.github.com>
2023-10-30 09:22:41 +0700

    WEBPRO-15
    Revisi

--- src/pages/DataAnggota/Anggota.jsx
+++ src/pages/DataAnggota/Anggota.jsx
@@ -1,8 +1,58 @@
+import { useState } from "react";
 import { Table, TableHeader, TableBody, TableRow, TableCol } from '../../components/Table';
 import profile from "../../assets/dummy-profile.jpg";
 import { MdOutlineDelete, MdMoreVert } from "react-icons/md";
+import { PopUpDialog, PopUpActions, PopUpContents, PopUpHeader } from "../../components/PopUpDialog";
+
+function NewMemberDialog({ showDialog, setShowDialog }) {
+    return (
+        <PopUpDialog open={showDialog} onChange={setShowDialog}>
+            <PopUpHeader text="New Member" />
+            <PopUpContents>
+                <div class="flex flex-row gap-4 items-center">
+                    <form class="flex flex-col gap-2 flex-1">
+                        <table class="w-full border-separate border-spacing-x-2 border-spacing-y-2">
+                            <tbody>
+                                <tr>
+                                    <td class="w-32">Name</td>
+                                    <td><input class="aseinput w-full" type="text" /></td>
+                                </tr>
+                                <tr>
+                                    <td className="w-32">Faculty</td>
+                                    <td><input class="aseinput w-full" type="text" /></td>
+                                </tr>
+                                <tr>
+                                    <td className="w-32">Major</td>
+                                    <td><input class="aseinput" type="date" /></td>
+                                </tr>
+                                <tr>
+                                    <td className="w-32">Entry Year</td>
+                                    <td><input class="aseinput" type="time" /></td>
+                                </tr>
+                                <tr>
+                                    <td className="w-32">Age</td>
+                                    <td><input class="aseinput w-full" type="text" /></td>
+                                </tr>
+                                <tr>
+                                    <td className="w-32">Picture</td>
+                                    <td><input class="aseinput w-full" type="file" /></td>
+                                </tr>
+                            </tbody>
+                        </table>
+                    </form>
+                </div>
+
+            </PopUpContents>
+            <PopUpActions>
+                <button className="py-1 px-4 rounded-full bg-aseorange text-white" onClick={() => { alert("Saved"); setShowDialog(false) }}>Save</button>
+                <button className="py-1 px-4 rounded-full bg-aseorange text-white" onClick={() => setShowDialog(false)}>Close</button>
+            </PopUpActions>
+        </PopUpDialog>
+    )
+}
 
 export default function Anggota() {
+    const [showDialog, setShowDialog] = useState(false);
     return (
         <div className="p-4 w-full flex flex-col" >
             <h1 className="text-3xl font-medium my-5 mx-5">Data Anggota</h1>
@@ -97,7 +147,8 @@ export default function Anggota() {
                     </TableRow>
                 </TableBody>
             </Table>
-                <button class="item-right self-end border-2 bg-white hover:bg-asegrey text-black font-normal px-9 py-1 my-10 rounded-full">New Member</button>
+            <button onClick={() => setShowDialog(true)} class="item-right self-end border-2 bg-white hover:bg-asegrey text-black font-normal px-9 py-1 my-10 rounded-full">New Member</button>
+            <NewMemberDialog showDialog={showDialog} setShowDialog={setShowDialog}/>
         </div>
     )
 }
