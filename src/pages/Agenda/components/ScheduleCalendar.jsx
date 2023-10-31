d3001444a21176c645264b8bc2354fd40275cb1f
nasrulilham <91175018+nasrulilham@users.noreply.github.com>
2023-10-31 20:31:09 +0700

    Agenda Done

--- src/pages/Agenda/components/ScheduleCalendar.jsx
+++ src/pages/Agenda/components/ScheduleCalendar.jsx
@@ -1,7 +1,6 @@
 import { useState } from "react";
-import { MdChevronLeft, MdChevronRight } from "react-icons/md";
 
-export default function ScheduleCalendar({ currentDate, onDateUpdate, readOnly }) {
+export default function ScheduleCalendar({ currentDate, onDateUpdate, readOnly, data }) {
   const [date, setDate] = useState(currentDate || new Date());
   currentDate = currentDate || new Date();
   function setCurrentDate(newDate) {
@@ -35,12 +34,20 @@ export default function ScheduleCalendar({ currentDate, onDateUpdate, readOnly }
       if (i <= firstDay) {
         days.push(<div key={i} className="text-gray-400 flex justify-center aspect-square">{prevTotalDays - (firstDay - i)}</div>);
       } else {
+        const events=[];
+        if(data[date.getMonth()+1]){
+          if(data[date.getMonth()+1][i-firstDay]){
+            data[date.getMonth()+1][i-firstDay].forEach(element => {
+              events.push(<span>{element.time} {element.nama}</span>)
+            });
+          }
+        }
         days.push(
           <div
             key={i}
-            className={"text-black flex  justify-center aspect-square rounded-full cursor-pointer w-full" }
-            onClick={() => updateDateSelection(i - firstDay)}
-          ><span className={"flex flex-row justify-center rounded-full items-center h-8 p-1 aspect-square "+ (viewCurrentMonth && i - firstDay === currentDate.getDate() ? " bg-green-400 font-medium" : "")}>{i - firstDay}</span></div>
+            className={"text-black flex  justify-center aspect-square rounded-full cursor-pointer w-full " }
+            onClick={() => updateDateSelection(i - firstDay)}>
+            <span className={"flex flex-row justify-center rounded-full items-center h-8 p-1 aspect-square "+ (viewCurrentMonth && i - firstDay === currentDate.getDate() ? " bg-green-400 font-medium " : "")}>{i - firstDay}</span><div class="absolute flex-col pt-10 font-light">{events}</div></div>
         );
       }
     }
@@ -53,7 +60,7 @@ export default function ScheduleCalendar({ currentDate, onDateUpdate, readOnly }
   };
 
   return (
-    <div className="w-120 h-fit text-center p-10 mx-2  bg-neutral-100 rounded-md font-light border">
+    <div className="h-fit text-center p-4 mx-32 my-4  bg-neutral-100 rounded-md font-light border">
       <div className="flex flex-row items-center mb-7 justify-between">
         <button onClick={() => setDate(new Date(date.getFullYear(), date.getMonth() - 1))} type="button" class="bg bg-aseorange border-aseorange text-white border p-2 font-light rounded-2xl px-4 w-32 h-fit ">Prev</button>
         {/*}
@@ -65,7 +72,7 @@ export default function ScheduleCalendar({ currentDate, onDateUpdate, readOnly }
         </div>
         <button onClick={() => setDate(new Date(date.getFullYear(), date.getMonth() + 1))} type="button" class="bg bg-aseorange border-aseorange text-white border p-2 font-light rounded-2xl px-4 w-32 h-fit">Next</button>
       </div>
-      <div className="grid grid-cols-7 gap-1 text-center mb-8">
+      <div className="grid grid-cols-7 gap-1 text-center">
         <div>Su</div>
         <div>Mo</div>
         <div>Tu</div>
